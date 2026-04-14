e22acde29f92dd6ee950749fb25cd959582856c5
Levi de Jesus <levijesus2121@gmail.com>
2026-04-14 16:10:35 -0300

    feat: Added built-in function to drag the object in the script, using EventySystems

---
+++ Assets/Scripts/DraggableItem.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+
+}
