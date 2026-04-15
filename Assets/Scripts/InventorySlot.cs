f6643278d2e48346676b93c851ed60300b11bde7
Levi de Jesus <levijesus2121@gmail.com>
2026-04-15 13:03:06 -0300

    feat: IT IS WORKING

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -5,6 +5,7 @@ public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
+
         InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
         inventoryItem.parentAfterDrag = transform;
 
